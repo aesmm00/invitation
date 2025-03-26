3001412060c03a36b43411726605692f76ae91d0
aesmm00 <aesmm18@gmail.com>
2025-03-27 03:04:47 +0800

    Add axios for data fetching in EighteenRoses and EighteenBlueBills components; refactor participant handling

--- src/Components/EighteenRoses/index.jsx
+++ src/Components/EighteenRoses/index.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, CardMedia, Container, Typography, List, ListItem } from '@mui/material';
 import eighteenRosesBG from '../../assets/photos/eighteenRoses/eighteenRosesBG.jpg';
 import eighteenSidePhoto from '../../assets/photos/eighteenRoses/eighteenSidePhoto.jpg';
-
-const participants = [
-    "Name1",
-    "Name2",
-    "Name3",
-    "Name4",
-    "Name5",
-    "Name6",
-    "Name7",
-    "Name8",
-    "Name9",
-    "Name10",
-    "Name11",
-    "Name12",
-    "Name13",
-    "Name14",
-    "Jomarie Sales",
-    "Elver Magante",
-    "Perseus Macavinta",
-    "Romano Magante"
-];
+import axios from 'axios';
 
 const EighteenRoses = () => {
-    const half = Math.ceil(participants.length / 2);
-    const firstHalf = participants.slice(0, half);
-    const secondHalf = participants.slice(half);
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState("");
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`https://script.google.com/macros/s/AKfycbw1T_Gf1k37jAYAhtC-ozc8QcsKrQ2ug79VvSPDMaScCc7Gm3EUhZw1eQQlOhE4OTHB/exec?sheet=EighteenRoses`);
+                if (response.data.error) {
+                    setError(response.data.error);
+                } else {
+                    setData(response.data);
+                    setError("");
+                }
+            } catch (error) {
+                setError("Error fetching data");
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const half = Math.ceil(data.length / 2);
+    const firstHalf = data.slice(0, half);
+    const secondHalf = data.slice(half);
 
     return (
         <Container maxWidth="2xl" style={{ padding:0, color: '#CF0A0A', backgroundImage: `url(${eighteenRosesBG})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', margin: 0 }} m={0}>
@@ -45,21 +47,26 @@ const EighteenRoses = () => {
                     <Typography variant="h5" paragraph letterSpacing={2}>
                         A waltz dance with 18 special men in the debutante’s life—symbolizing love and guidance as she steps into adulthood.
                     </Typography>
+                    {error && (
+                        <Typography variant="h6" color="error" paragraph>
+                            {error}
+                        </Typography>
+                    )}
                     <Box sx={{ display: 'flex', flexDirection: {xs: 'column', sm: 'row'}}}>
                         <List sx={{ pl: {xs: 1, sm: 8} }} >
-                            {firstHalf.map((name, index) => (
+                            {firstHalf.map((item, index) => (
                                 <ListItem key={index} dense style={{padding:0}}>
                                     <Typography variant="h6" paragraph letterSpacing={3}>
-                                        {index+1} - {name}
+                                        {index + 1} - {item.Name}
                                     </Typography>
                                 </ListItem>
                             ))}
                         </List>
                         <List sx={{ pl: {xs: 1, sm: 15} }} >
-                            {secondHalf.map((name, index) => (
+                            {secondHalf.map((item, index) => (
                                 <ListItem key={index + half} dense style={{padding:0}}>
                                     <Typography variant="h6" paragraph letterSpacing={3}>
-                                        {index + half + 1} - {name}
+                                        {index + half + 1} - {item.Name}
                                     </Typography>
                                 </ListItem>
                             ))}
